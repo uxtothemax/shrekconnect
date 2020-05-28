334337c7aa54b0ffea5d508055d30d26ba0ce62e
uxtothemax <romy.wiedijk@hva.nl>
2020-05-28 10:27:24 +0200

    -A

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// const express = require('express');
-// const app = express();
-// const port = 8090;
-//
-// //ipv express().get()
-// app.get('/', home);
-// app.get('/about', about);
-//
-// function home(req, res) {
-//   res.send('Hello World!');
-// }
-//
-// function about(req, res) {
-//   res.send('About sectieeee');
-// }
-//
-// app.listen(port, function() {
-//   console.log('Server is running, awesome!')
-// })
+const express = require('express')
+const app = express()
+const port = 8090;
+
+//ipv express().get()
+app.get('/', home);
+app.get('/about', about)
+
+function home(req, res) {
+  res.send('Hello World!')
+}
+
+function about(req, res) {
+  res.send('About sectieeee')
+}
+
+app.listen(port, function() {
+  console.log('Server is running, awesome!')
+})
